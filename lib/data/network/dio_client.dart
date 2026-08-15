3a4999247bc9986f41b826b8a42ec05c823da192
lana-ab <lanaabokhier2222@gmail.com>
2026-08-15 18:33:26 +0300

    Save dio client changes

--- lib/data/network/dio_client.dart
+++ lib/data/network/dio_client.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:school_management/utils/shared_prefs_helper.dart';
 
 class DioClient {
-<<<<<<< HEAD
   static late Dio dio;
 
   static Future<void> init() async {
@@ -22,33 +21,21 @@ class DioClient {
       InterceptorsWrapper(
         onRequest: (options, handler) async {
           final token = await SharedPrefsHelper.getToken();
+
           if (token != null) {
             options.headers['Authorization'] = 'Bearer $token';
           }
+
           return handler.next(options);
         },
         onError: (error, handler) {
           if (error.response?.statusCode == 401) {
             SharedPrefsHelper.clearToken();
           }
+
           return handler.next(error);
         },
       ),
     );
   }
-=======
-  static final Dio dio = Dio(
-    BaseOptions(
-      baseUrl: 'http://127.0.0.1:8000/api/v1',
-      connectTimeout: const Duration(seconds: 30),
-      receiveTimeout: const Duration(seconds: 30),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization':
-            'Bearer 1|Eogvj31z6VKcr1uTWp9rOhytQ6mb0xxibrKnRBV57f346347',
-      },
-    ),
-  );
->>>>>>> lana
 }
